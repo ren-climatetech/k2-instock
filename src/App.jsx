73b6fa8276fc6cc51d81ca1d8d5940895d5e7288
Elizabeth <lizagotovitryby@gmail.com>
2024-11-13 12:01:27 -0500

    fix App.jsx syntax

--- src/App.jsx
+++ src/App.jsx
@@ -8,29 +8,30 @@ import EditWarehouseItemPage from './pages/EditWarehouseItemPage/EditWarehouseIt
 import AddWarehousePage from './pages/AddWarehousePage/AddWarehousePage.jsx';
 import AddInventoryPage from './pages/AddInventoryPage/AddInventoryPage.jsx';
 
-import './App.css'
+import './App.scss'
 
 function App() {
-    return (
-      <BrowserRouter>
-        <Routes>
-          { /* Main pages */}
-          <Route path="/warehouse" element={ <WarehousePage/> } />
-          <Route path="/inventory" element= { <InventoryPage/> } />
+  return (
+    <BrowserRouter>
+      <Routes>
+        { /* Main pages */}
+        <Route path="/warehouse" element={<WarehousePage />} />
+        <Route path="/inventory" element={<InventoryPage />} />
 
-          {/* Item Pages */}
-          <Route path="/warehouse/:itemId" element={ <WarehouseItemPage/> } />
-          <Route path="/inventory/:itemId" element={ <InventoryItemPage/> } />
+        {/* Item Pages */}
+        <Route path="/warehouse/:itemId" element={<WarehouseItemPage />} />
+        <Route path="/inventory/:itemId" element={<InventoryItemPage />} />
 
-          {/* Edit Pages */}
-          <Route path="/warehouse/:itemId/edit" element={ <EditWarehouseItemPage/> } />
-          <Route path="/inventory/:itemId/edit" element={ <EditInventoryItemPage/> } />
+        {/* Edit Pages */}
+        <Route path="/warehouse/:itemId/edit" element={<EditWarehouseItemPage />} />
+        <Route path="/inventory/:itemId/edit" element={<EditInventoryItemPage />} />
 
-          {/* Add Pages */}
-          <Route path="/warehouse/add" element={ <AddWarehousePage/> } />
-          <Route path="/inventory/add" element={ <AddInventoryPage/> } />
+        {/* Add Pages */}
+        <Route path="/warehouse/add" element={<AddWarehousePage />} />
+        <Route path="/inventory/add" element={<AddInventoryPage />} />
 
-        </Routes>
-      </BrowserRouter>
-    )
+      </Routes>
+    </BrowserRouter>
+  )
+}
 export default App;
