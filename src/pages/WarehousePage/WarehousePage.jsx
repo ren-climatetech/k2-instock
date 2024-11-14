206bddac860e62ba230be7d849b06ec7c4a18646
Elizabeth <lizagotovitryby@gmail.com>
2024-11-14 17:55:57 -0500

    Update Warehouse page with title, buttons. Styles for all breakpoints

--- src/pages/WarehousePage/WarehousePage.jsx
+++ src/pages/WarehousePage/WarehousePage.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import TextButton from "../../components/Buttons/AddButton/AddButton";
+import Search from "../../components/Search/Search";
+import "./WarehousePage.scss";
 
 function WarehousePage() {
   return (
-    <div>
-      Hello, I am warehouse page
-    </div>
+    <section>
+      <div className="layout warehouses__container">
+        <div className="warehouses__heading-and_buttons">
+        <h1 className="warehouses__title">Warehouses</h1>
+        <div className="warehouses__buttons">
+        <Search />
+        <TextButton text="+ Add New Warehouse" />
+        </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
