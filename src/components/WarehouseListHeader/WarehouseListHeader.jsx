fd41ae1490ad546fab18f60ded0379d9bb768874
Elizabeth <lizagotovitryby@gmail.com>
2024-11-15 12:55:13 -0500

    Created warehouses list, fetch list from backend, styled list for all breakpoints

--- src/components/WarehouseListHeader/WarehouseListHeader.jsx
+++ src/components/WarehouseListHeader/WarehouseListHeader.jsx
@@ -1,20 +1,30 @@
 import "./WarehouseListHeader.scss";
+import sortIcon from "../../assets/icons/sort-icon-default.svg";
 
 const WarehouseListHeader = () => {
   return (
     <div className="warehouses-header">
+
       <div className="warehouses-header__name-container">
-        <p className="warehouses-header__name">Warehouse</p>
+        <p className="warehouses-header__name">
+          Warehouse
+          <img src={sortIcon} alt="sort icon" />
+        </p>
       </div>
       <div className="warehouses-header__name-container">
         <p className="warehouses-header__name">Address</p>
+        <img src={sortIcon} alt="sort icon" />
       </div>
       <div className="warehouses-header__name-container">
         <p className="warehouses-header__name">Contact name</p>
+
+        <img src={sortIcon} alt="sort icon" />
       </div>
       <div className="warehouses-header__name-container">
         <p className="warehouses-header__name">contact information</p>
+        <img src={sortIcon} alt="sort icon" />
       </div>
+
       <div>
         <p>actions</p>
       </div>
--- src/pages/WarehousePage/WarehousePage.jsx
+++ src/pages/WarehousePage/WarehousePage.jsx
@@ -1,9 +1,28 @@
+import { useEffect, useState } from "react";
 import AddButton from "../../components/Buttons/AddButton/AddButton";
 import Search from "../../components/Search/Search";
 import WarehouseListHeader from "../../components/WarehouseListHeader/WarehouseListHeader";
 import "./WarehousePage.scss";
+import axios from "axios";
+import WarehousesList from "../../components/WarehousesList/WarehousesList";
 
 function WarehousePage() {
+  const [warehouses, setWarehouses] = useState([]);
+
+  async function getWarehouses() {
+     try {
+       const {data}= await axios.get("http://localhost:8080/api/warehouses");
+       console.log(data);
+       setWarehouses(data);
+     } catch (error) {
+       console.error(error);
+     }
+  }
+  
+  useEffect(() => {
+    getWarehouses()
+  }, [])
+
   return (
     <section>
       <div className="layout warehouses__container">
@@ -17,6 +36,7 @@ function WarehousePage() {
         </div>
         {/* Warehouse LIst Header */}
         <WarehouseListHeader />
+        <WarehousesList warehouses={warehouses}/>
       </div>
      
     </section>
