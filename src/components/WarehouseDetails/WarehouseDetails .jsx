0a0f0d9a211c299ad52a8e24a96a14d352a8b09c
Zuri Gomez <zgo522@gmail.com>
2024-11-16 00:08:40 -0500

    Update .env and component with correct url; test functionality, and add resusable edit button.

--- src/components/WarehouseDetails/WarehouseDetails .jsx
+++ src/components/WarehouseDetails/WarehouseDetails .jsx
@@ -2,31 +2,33 @@ import React from "react";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
+import EditItemButton from "../Buttons/EditItemButton/EditItemButton";
+import "../WarehouseDetails/WarehouseDetails.scss";
 
-const BASE_URL = import.meta.env.VITE_API_URL;
+const BASE_URL = import.meta.env.VITE_BACKEND_URL;
 
 const WarehouseDetails = () => {
   const { id } = useParams();
   const [warehouse, setWarehouse] = useState({
-    name: "",
+    warehouse_name: "",
     address: "",
-    contactName: "",
-    contactPosition: "", 
-    contactPhone: "", 
-    contactEmail: "",
+    contact_name: "",
+    contact_position: "", 
+    contact_phone: "", 
+    contact_email: "",
   });
 
   const getWarehouse = async () => {
     try {
       const response = await axios.get(
-        `${BASE_URL}/warehouses/${id}`);
+        `${BASE_URL}/api/warehouses/${id}`);
       setWarehouse({
-        name: response.data.warehouse_name,
+        warehouse_name: response.data.warehouse_name,
         address: response.data.address,
-        contactName: response.data.contact_name,
-        contactPosition: response.data.contact_position,
-        contactPhone: response.data.contact_phone,
-        contactEmail: response.data.contact_email,
+        contact_name: response.data.contact_name,
+        contact_position: response.data.contact_position,
+        contact_phone: response.data.contact_phone,
+        contact_email: response.data.contact_email,
       });
     } catch (error) {
       console.error("Error fetching warehouse details:", error);
@@ -38,29 +40,26 @@ const WarehouseDetails = () => {
   }, [id]);
 
   return (
-    <div>
+    <section className="warehouse-details__container">
       <div>
         <h1>{warehouse.warehouse_name}</h1>
+        <EditItemButton/>
       </div>
-      <section className="warehouse-details">
+      <div className="warehouse-details">
         <div>
           <h3>WAREHOUSE ADDRESS:</h3>
           <p>{warehouse.address}</p>
         </div>
         <div>
-          <div>
-            <h3>CONTACT NAME:</h3>
-            <p>{warehouse.contactName}</p>
-            <p>{warehouse.contactPosition}</p>
-          </div>
-          <div>
-            <h3>CONTACT INFORMATION:</h3>
-            <p>{warehouse.contactPhone}</p>
-            <p>{warehouse.contactEmail} </p>
-          </div>
+          <h3>CONTACT NAME:</h3>
+          <p>{warehouse.contact_name}</p>
+          <p>{warehouse.contact_position}</p>
+          <h3>CONTACT INFORMATION:</h3>
+          <p>{warehouse.contact_phone}</p>
+          <p>{warehouse.contact_email} </p>
         </div>
-      </section>
-    </div>
+      </div>
+    </section>
   );
 };
 
