821df5e94acaf7a507179b0f783c8cfb57815c7f
katrinsukovati <katrin.sukovati1@gmail.com>
2024-11-16 13:49:23 -0500

    adding id to delete message and updated env file to use VITE_BACKEND_URL variable and also added env.sample file

--- src/components/WarehousesList/WarehousesList.jsx
+++ src/components/WarehousesList/WarehousesList.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 import "./WarehousesList.scss";
 import DeleteFromListButton from "../Buttons/DeleteFromListButton.js/DeleteFromListButton";
 import EditFromListButton from "../Buttons/EditFromListButton/EditFromListButton";
-import chevronIcon from "../../assets/icons/chevron_right-24px.svg"
+import chevronIcon from "../../assets/icons/chevron_right-24px.svg";
 import DeleteModal from "../DeleteModal/DeleteModal";
 import axios from "axios";
 import { toast } from "react-toastify";
@@ -15,17 +15,17 @@ const WarehousesList = () => {
   const [warehouses, setWarehouses] = useState([]);
 
   async function getWarehouses() {
-     try {
-       const {data}= await axios.get(`${baseUrl}/api/warehouses`);
-       setWarehouses(data);
-     } catch (error) {
-       console.error(error);
-     }
+    try {
+      const { data } = await axios.get(`${baseUrl}/api/warehouses`);
+      setWarehouses(data);
+    } catch (error) {
+      console.error(error);
+    }
   }
-  
+
   useEffect(() => {
-    getWarehouses()
-  }, [])
+    getWarehouses();
+  }, []);
 
   const openModal = (warehouse) => {
     setSelectedWarehouse(warehouse);
@@ -40,80 +40,94 @@ const WarehousesList = () => {
   const handleDelete = async () => {
     if (selectedWarehouse) {
       try {
-        const response = await axios.delete(`${baseUrl}/api/warehouses/${selectedWarehouse.id}`);
+        const response = await axios.delete(
+          `${baseUrl}/api/warehouses/${selectedWarehouse.id}`
+        );
 
         if (response.status === 204) {
           getWarehouses();
           closeModal();
-          toast.success(`${selectedWarehouse.warehouse_name} with ${selectedWarehouse.id} was successfully deleted.`);
+          toast.success(
+            `${selectedWarehouse.warehouse_name} with ID ${selectedWarehouse.id} was successfully deleted.`
+          );
         } else {
-          toast.error(`${selectedWarehouse.warehouse_name} with ${selectedWarehouse.id} couldn't be deleted.`)
+          toast.error(
+            `${selectedWarehouse.warehouse_name} with ID ${selectedWarehouse.id} couldn't be deleted.`
+          );
         }
       } catch (error) {
-        toast.error(`${selectedWarehouse.warehouse_name} with ${selectedWarehouse.id} couldn't be deleted. ${error.response?.data?.message}`)
+        toast.error(
+          `${selectedWarehouse.warehouse_name} with ID ${selectedWarehouse.id} couldn't be deleted. ${error.response?.data?.message}`
+        );
         console.error("Error:", error);
       }
     }
-     
   };
   return (
     <>
-    <ul className="warehouses__list">
-      {warehouses.map(
-        ({
-          id,
-          warehouse_name,
-          address,
-          city,
-          contact_email,
-          contact_phone,
-        }) => {
-          return (
-            <li key={id} className="warehouses__item">
-                  <div className="warehouses__info-link">
-                <div className="warehouses__content">
-                  <span className="warehouses__label">Warehouse</span>
-                  <Link to={`/warehouse/${id}`} className="warehouses__warehouse">
-                    {warehouse_name}
-                    <img
-                      src={chevronIcon}
-                      alt="arrow icon"
-                      className="warehouses__warehouse-icon"
-                    />
-                  </Link>
+      <ul className="warehouses__list">
+        {warehouses.map(
+          ({
+            id,
+            warehouse_name,
+            address,
+            city,
+            contact_email,
+            contact_phone,
+          }) => {
+            return (
+              <li key={id} className="warehouses__item">
+                <div className="warehouses__info-link">
+                  <div className="warehouses__content">
+                    <span className="warehouses__label">Warehouse</span>
+                    <Link
+                      to={`/warehouse/${id}`}
+                      className="warehouses__warehouse"
+                    >
+                      {warehouse_name}
+                      <img
+                        src={chevronIcon}
+                        alt="arrow icon"
+                        className="warehouses__warehouse-icon"
+                      />
+                    </Link>
+                  </div>
+                  <div className="warehouses__content warehouses__content-address">
+                    <span className="warehouses__label">address</span>
+                    <p className="warehouses__address">{address}</p>
+                  </div>
+                  <div className="warehouses__content">
+                    <span className="warehouses__label">contact name</span>
+                    <p> {city}</p>
+                  </div>
+                  <div className="warehouses__content warehouses__content-contact">
+                    <span className="warehouses__label">
+                      contact information
+                    </span>
+                    <p>{contact_phone}</p>
+                    <p>{contact_email}</p>
+                  </div>
                 </div>
-                <div className="warehouses__content warehouses__content-address">
-                  <span className="warehouses__label">address</span>
-                  <p className="warehouses__address">{address}</p>
+                <div className="action-buttons">
+                  <DeleteFromListButton
+                    onClick={() => openModal({ id, warehouse_name })}
+                  />
+                  <EditFromListButton path={`/warehouse/${id}/edit`} />
                 </div>
-                <div className="warehouses__content">
-                  <span className="warehouses__label">contact name</span>
-                  <p> {city}</p>
-                </div>
-                <div className="warehouses__content warehouses__content-contact">
-                  <span className="warehouses__label">contact information</span>
-                  <p>{contact_phone}</p>
-                  <p>{contact_email}</p>
-                </div>
-              </div>
-              <div className="action-buttons">
-                <DeleteFromListButton onClick={() => openModal({ id, warehouse_name })}/>
-                <EditFromListButton path={`/warehouse/${id}/edit`} />
-              </div>
-            </li>
-          );
-        }
-      )}
-    </ul>
-    {isModalOpen && (
-      <DeleteModal
-        isOpen={isModalOpen}
-        onClose={closeModal}
-        onDelete={handleDelete}
+              </li>
+            );
+          }
+        )}
+      </ul>
+      {isModalOpen && (
+        <DeleteModal
+          isOpen={isModalOpen}
+          onClose={closeModal}
+          onDelete={handleDelete}
           itemName={selectedWarehouse?.warehouse_name}
           category={"Warehouse"}
-      />
-    )}
+        />
+      )}
     </>
   );
 };
