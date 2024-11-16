43ae1730ef7f4e296d5c13b38de62c134b6eba6f
Zuri Gomez <zgo522@gmail.com>
2024-11-15 22:15:52 -0500

    Complete styles for tablet and desktop screens

--- src/components/AddWarehouseForm/AddWarehouseForm.jsx
+++ src/components/AddWarehouseForm/AddWarehouseForm.jsx
@@ -94,109 +94,111 @@ const AddWarehouseForm = () => {
 
   return (
     <form className="add-warehouse__form" onSubmit={handleSubmit}>
-      <div className="add-warehouse__form_warehouse">
-        <h2 className="add-warehouse__form_warehouse-title">
-          Warehouse Details
-        </h2>
-        <label className="add-warehouse__form_warehouse-label">
-          Warehouse Name
-        </label>
-        <input
-          className="add-warehouse__form_warehouse-input"
-          type="text"
-          name="warehouse_name"
-          value={form.warehouse_name}
-          placeholder="Warehouse Name"
-          onChange={handleChange}
-        />
-        {errors.warehouse_name && <span>{errors.warehouse_name}</span>}
-        <label className="add-warehouse__form_warehouse-label">
-          Street Address
-        </label>{" "}
-        <input
-          className="add-warehouse__form_warehouse-input"
-          type="text"
-          name="address"
-          value={form.address}
-          placeholder="Street Address"
-          onChange={handleChange}
-        />
-        {errors.address && <span>{errors.address}</span>}
-        <label className="add-warehouse__form_warehouse-label">City</label>
-        <input
-          className="add-warehouse__form_warehouse-input"
-          type="text"
-          name="city"
-          value={form.city}
-          placeholder="City"
-          onChange={handleChange}
-        />
-        {errors.city && <span>{errors.city}</span>}
-        <label className="add-warehouse__form_warehouse-label">
-          Country
-        </label>{" "}
-        <input
-          className="add-warehouse__form_warehouse-input"
-          type="text"
-          name="country"
-          value={form.country}
-          placeholder="Country"
-          onChange={handleChange}
-        />
-        {errors.country && <span>{errors.country}</span>}
-      </div>
-      <div className="add-warehouse__form_contact">
-        <h2 className="add-warehouse__form_contact-title">Contact Details</h2>
-        <label className="add-warehouse__form_contact-label">
-          Contact Name
-        </label>
-        <input
-          className="add-warehouse__form_contact-input"
-          type="text"
-          name="contact_name"
-          value={form.contact_name}
-          placeholder="Contact Name"
-          onChange={handleChange}
-        />
-        {errors.contact_name && <span>{errors.contact_name}</span>}
-        <label className="add-warehouse__form_contact-label">
-          Contact Position
-        </label>
-        <input
-          className="add-warehouse__form_contact-input"
-          type="text"
-          name="contact_position"
-          value={form.contact_position}
-          placeholder="Position"
-          onChange={handleChange}
-        />
-        {errors.contact_position && <span>{errors.contact_position}</span>}
-        <label className="add-warehouse__form_contact-label">
-          Contact Phone
-        </label>
-        <input
-          className="add-warehouse__form_contact-input"
-          type="text"
-          name="contact_phone"
-          value={form.contact_phone}
-          placeholder="Phone Number"
-          onChange={handleChange}
-        />
-        {errors.contact_phone && <span>{errors.contact_phone}</span>}
-        <label className="add-warehouse__form_contact-label">
-          Contact Email
-        </label>
-        <input
-          className="add-warehouse__form_contact-input"
-          type="text"
-          name="contact_email"
-          value={form.contact_email}
-          placeholder="Email"
-          onChange={handleChange}
-        />
-        {errors.contact_email && <span>{errors.contact_email}</span>}
-      </div>
-      <div className="add-warehouse__form_buttons">
+      <div className="add-warehouse__form-wrapper">
+        <div className="add-warehouse__form-wrapper_warehouse">
+          <h2 className="add-warehouse__form-wrapper_warehouse-title">
+            Warehouse Details
+          </h2>
+          <label className="add-warehouse__form-wrapper_warehouse-label">
+            Warehouse Name
+          </label>
+          <input
+            className="add-warehouse__form-wrapper_warehouse-input"
+            type="text"
+            name="warehouse_name"
+            value={form.warehouse_name}
+            placeholder="Warehouse Name"
+            onChange={handleChange}
+          />
+          {errors.warehouse_name && <span>{errors.warehouse_name}</span>}
+          <label className="add-warehouse__form-wrapper_warehouse-label">
+            Street Address
+          </label>{" "}
+          <input
+            className="add-warehouse__form-wrapper_warehouse-input"
+            type="text"
+            name="address"
+            value={form.address}
+            placeholder="Street Address"
+            onChange={handleChange}
+          />
+          {errors.address && <span>{errors.address}</span>}
+          <label className="add-warehouse__form-wrapper_warehouse-label">City</label>
+          <input
+            className="add-warehouse__form-wrapper_warehouse-input"
+            type="text"
+            name="city"
+            value={form.city}
+            placeholder="City"
+            onChange={handleChange}
+          />
+          {errors.city && <span>{errors.city}</span>}
+          <label className="add-warehouse__form-wrapper_warehouse-label">
+            Country
+          </label>{" "}
+          <input
+            className="add-warehouse__form-wrapper_warehouse-input"
+            type="text"
+            name="country"
+            value={form.country}
+            placeholder="Country"
+            onChange={handleChange}
+          />
+          {errors.country && <span>{errors.country}</span>}
+        </div>
+        <div className="add-warehouse__form-wrapper_contact">
+          <h2 className="add-warehouse__form-wrapper_contact-title">Contact Details</h2>
+          <label className="add-warehouse__form-wrapper_contact-label">
+            Contact Name
+          </label>
+          <input
+            className="add-warehouse__form-wrapper_contact-input"
+            type="text"
+            name="contact_name"
+            value={form.contact_name}
+            placeholder="Contact Name"
+            onChange={handleChange}
+          />
+          {errors.contact_name && <span>{errors.contact_name}</span>}
+          <label className="add-warehouse__form-wrapper_contact-label">
+            Contact Position
+          </label>
+          <input
+            className="add-warehouse__form-wrapper_contact-input"
+            type="text"
+            name="contact_position"
+            value={form.contact_position}
+            placeholder="Position"
+            onChange={handleChange}
+          />
+          {errors.contact_position && <span>{errors.contact_position}</span>}
+          <label className="add-warehouse__form-wrapper_contact-label">
+            Contact Phone
+          </label>
+          <input
+            className="add-warehouse__form-wrapper_contact-input"
+            type="text"
+            name="contact_phone"
+            value={form.contact_phone}
+            placeholder="Phone Number"
+            onChange={handleChange}
+          />
+          {errors.contact_phone && <span>{errors.contact_phone}</span>}
+          <label className="add-warehouse__form-wrapper_contact-label">
+            Contact Email
+          </label>
+          <input
+            className="add-warehouse__form-wrapper_contact-input"
+            type="text"
+            name="contact_email"
+            value={form.contact_email}
+            placeholder="Email"
+            onChange={handleChange}
+          />
+          {errors.contact_email && <span>{errors.contact_email}</span>}
+        </div>
+      </div> 
+      <div className="add-warehouse__form-buttons">
         <CancelButton onClick={handleCancel}/>
         <AddSubmitButton text="+Add Warehouse"/>
       </div>
