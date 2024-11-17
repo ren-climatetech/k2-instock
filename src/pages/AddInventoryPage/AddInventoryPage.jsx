824db36f16699b75366aa5c1a606910d2c5c055f
Zuri Gomez <zgo522@gmail.com>
2024-11-16 21:36:55 -0500

    Add style to addinventorypage

--- src/pages/AddInventoryPage/AddInventoryPage.jsx
+++ src/pages/AddInventoryPage/AddInventoryPage.jsx
@@ -1,12 +1,22 @@
-import React from 'react'
-import AddInventoryForm from '../../components/AddInventoryForm/AddInventoryForm'
+import React from "react";
+import AddInventoryForm from "../../components/AddInventoryForm/AddInventoryForm";
+import GoBackButton from "../../components/Buttons/GoBackButton/GoBackButton";
+import "../AddInventoryPage/AddInventoryPage.scss"
 
 function AddInventoryPage() {
   return (
-    <div>
-      <AddInventoryForm/>
-    </div>
-  )
+    <section className="layout">
+      <div className="add-inventory__wrapper">
+        <div className="add-inventory__wrapper-header">
+          <GoBackButton path="/inventories" />
+          <h1 className="add-inventory_wrapper-header-title">
+            Add New Inventory Item
+          </h1>
+        </div>
+        <AddInventoryForm />
+      </div>
+    </section>
+  );
 }
 
-export default AddInventoryPage
+export default AddInventoryPage;
