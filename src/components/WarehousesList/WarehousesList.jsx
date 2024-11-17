d788721b69c0ddb993ec3e026ea797a7cc6f962a
katrinsukovati <katrin.sukovati1@gmail.com>
2024-11-17 16:01:32 -0500

    search for warehouse page

--- src/components/WarehousesList/WarehousesList.jsx
+++ src/components/WarehousesList/WarehousesList.jsx
@@ -6,26 +6,13 @@ import chevronIcon from "../../assets/icons/chevron_right-24px.svg";
 import DeleteModal from "../DeleteModal/DeleteModal";
 import axios from "axios";
 import { toast } from "react-toastify";
-import { useEffect, useState } from "react";
+import { useState } from "react";
+
 const baseUrl = import.meta.env.VITE_BACKEND_URL;
 
-const WarehousesList = () => {
+const WarehousesList = ({ warehouses, setWarehouses }) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [selectedWarehouse, setSelectedWarehouse] = useState(null);
-  const [warehouses, setWarehouses] = useState([]);
-
-  async function getWarehouses() {
-    try {
-      const { data } = await axios.get(`${baseUrl}/api/warehouses`);
-      setWarehouses(data);
-    } catch (error) {
-      console.error(error);
-    }
-  }
-
-  useEffect(() => {
-    getWarehouses();
-  }, []);
 
   const openModal = (warehouse) => {
     setSelectedWarehouse(warehouse);
@@ -45,7 +32,12 @@ const WarehousesList = () => {
         );
 
         if (response.status === 204) {
-          getWarehouses();
+          // Remove the deleted warehouse from the state
+          setWarehouses((prevWarehouses) =>
+            prevWarehouses.filter(
+              (warehouse) => warehouse.id !== selectedWarehouse.id
+            )
+          );
           closeModal();
           toast.success(
             `${selectedWarehouse.warehouse_name} with ID ${selectedWarehouse.id} was successfully deleted.`
@@ -63,6 +55,7 @@ const WarehousesList = () => {
       }
     }
   };
+
   return (
     <>
       <ul className="warehouses__list">
@@ -74,49 +67,45 @@ const WarehousesList = () => {
             city,
             contact_email,
             contact_phone,
-          }) => {
-            return (
-              <li key={id} className="warehouses__item">
-                <div className="warehouses__info-link">
-                  <div className="warehouses__content">
-                    <span className="warehouses__label">Warehouse</span>
-                    <Link
-                      to={`/warehouse/${id}`}
-                      className="warehouses__warehouse"
-                    >
-                      {warehouse_name}
-                      <img
-                        src={chevronIcon}
-                        alt="arrow icon"
-                        className="warehouses__warehouse-icon"
-                      />
-                    </Link>
-                  </div>
-                  <div className="warehouses__content warehouses__content-address">
-                    <span className="warehouses__label">address</span>
-                    <p className="warehouses__address">{address}</p>
-                  </div>
-                  <div className="warehouses__content">
-                    <span className="warehouses__label">contact name</span>
-                    <p> {city}</p>
-                  </div>
-                  <div className="warehouses__content warehouses__content-contact">
-                    <span className="warehouses__label">
-                      contact information
-                    </span>
-                    <p>{contact_phone}</p>
-                    <p>{contact_email}</p>
-                  </div>
+          }) => (
+            <li key={id} className="warehouses__item">
+              <div className="warehouses__info-link">
+                <div className="warehouses__content">
+                  <span className="warehouses__label">Warehouse</span>
+                  <Link
+                    to={`/warehouse/${id}`}
+                    className="warehouses__warehouse"
+                  >
+                    {warehouse_name}
+                    <img
+                      src={chevronIcon}
+                      alt="arrow icon"
+                      className="warehouses__warehouse-icon"
+                    />
+                  </Link>
                 </div>
-                <div className="action-buttons">
-                  <DeleteFromListButton
-                    onClick={() => openModal({ id, warehouse_name })}
-                  />
-                  <EditFromListButton path={`/warehouse/${id}/edit`} />
+                <div className="warehouses__content warehouses__content-address">
+                  <span className="warehouses__label">Address</span>
+                  <p className="warehouses__address">{address}</p>
                 </div>
-              </li>
-            );
-          }
+                <div className="warehouses__content">
+                  <span className="warehouses__label">City</span>
+                  <p>{city}</p>
+                </div>
+                <div className="warehouses__content warehouses__content-contact">
+                  <span className="warehouses__label">Contact Info</span>
+                  <p>{contact_phone}</p>
+                  <p>{contact_email}</p>
+                </div>
+              </div>
+              <div className="action-buttons">
+                <DeleteFromListButton
+                  onClick={() => openModal({ id, warehouse_name })}
+                />
+                <EditFromListButton path={`/warehouse/${id}/edit`} />
+              </div>
+            </li>
+          )
         )}
       </ul>
       {isModalOpen && (
