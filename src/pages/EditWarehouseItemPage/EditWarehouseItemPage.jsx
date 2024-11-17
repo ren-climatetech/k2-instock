f74c29879a9b924b5446ce781b6d2cc8df74a6d5
Ren <reine.donnestad@gmail.com>
2024-11-16 22:28:29 -0500

    initial setup for warehouse edit page

--- src/pages/EditWarehouseItemPage/EditWarehouseItemPage.jsx
+++ src/pages/EditWarehouseItemPage/EditWarehouseItemPage.jsx
@@ -1,11 +1,77 @@
-import React from 'react'
+import "./EditWarehouseItemPage.scss";
+import CancelButton from "../../components/Buttons/CancelButton/CancelButton";
+import SaveButton from "../../components/Buttons/SaveButton/SaveButton";
+import axios from "axios";
+import arrowBack from "../../assets/icons/arrow_back-24px.svg";
+import React from "react";
 
 function EditWarehouseItemPage() {
   return (
-    <div>
-      Hello, I am an Edit Warehouse Item Page
-    </div>
-  )
+    <>
+      <form>
+        <div className="editwarehouse__header">
+          <img
+            src={arrowBack}
+            alt="arrow icon"
+            className="editinventory__icon-arrow"
+          />
+          <h1>Edit Warehouse</h1>
+        </div>
+
+        <div>
+          <h2 className="editwarehouse__header-details">Warehouse Details</h2>
+          <div className="editwarehouse__name">
+            <h3>Warehouse Name</h3>
+            <input className="editwarehouse__entry" type="text"></input>
+          </div>
+          <div className="editwarehouse__street">
+            <h3>Street Address</h3>
+            <input className="editwarehouse__entry" type="text"></input>
+          </div>
+          <div className="editwarehouse__city">
+            <h3>City</h3>
+            <input className="editwarehouse__entry" type="text"></input>
+          </div>
+          <div className="editwarehouse__city">
+            <h3>City</h3>
+            <input className="editwarehouse__entry" type="text"></input>
+          </div>
+          <div className="editwarehouse__country">
+            <h3>Country</h3>
+            <input className="editwarehouse__entry" type="text"></input>
+          </div>
+        </div>
+
+        <div>
+          <h2 className="editcontact__header-details">Contact Details</h2>
+          <div className="editcontact__name">
+            <h3>Warehouse Name</h3>
+            <input className="editcontact__entry" type="text"></input>
+          </div>
+          <div className="editcontact__street">
+            <h3>Street Address</h3>
+            <input className="editcontact__entry" type="text"></input>
+          </div>
+          <div className="editcontact__city">
+            <h3>City</h3>
+            <input className="editcontact__entry" type="text"></input>
+          </div>
+          <div className="editcontact__city">
+            <h3>City</h3>
+            <input className="editcontact__entry" type="text"></input>
+          </div>
+          <div className="editcontact__country">
+            <h3>Country</h3>
+            <input className="editcontact__entry" type="text"></input>
+          </div>
+        </div>
+        
+
+
+
+      </form>
+    </>
+  );
 }
 
-export default EditWarehouseItemPage
+export default EditWarehouseItemPage;
